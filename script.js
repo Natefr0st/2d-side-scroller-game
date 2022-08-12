6d8a09b523f2dce852004c2fd5dfa0822cb5d002
Kevin <kevin_zino@yahoo.com>
2022-08-12 23:49:29 +0300

    Introduced game entities

--- script.js
+++ script.js
@@ -6,4 +6,32 @@ window.addEventListener("load", function () {
     const ctx = canvas.getContext("2d");
     canvas.width = 1500;
     canvas.height = 500;
+
+    class InputHandler {
+
+    }
+    class Projectile {
+
+    }
+    class Particle {
+
+    }
+    class Player {
+
+    }
+    class Enemy {
+
+    }
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+        
+    }
 });
