d066bbccfd493d91c90faef3c7aeab7c33ac57e2
Kevin <kevin_zino@yahoo.com>
2022-08-12 23:43:05 +0300

    Initial setup

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function () {
+    /**
+     * Canvas Setup
+     */
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 1500;
+    canvas.height = 500;
+});
