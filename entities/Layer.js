419019334b0c1c951c5652e71919e8cebb84a2d8
Kevin <kevin_zino@yahoo.com>
2022-08-18 00:34:40 +0300

    Added animated background parallax

--- entities/Layer.js
+++ entities/Layer.js
@@ -1,3 +1,22 @@
 export class Layer {
+    constructor(game, image, speedModifer) {
+        this.game = game;
+        this.image = image;
+        this.speedModifer = speedModifer;
+        this.width = 1768;
+        this.height = 500;
+        this.x = 0;
+        this.y = 0;
+    }
 
+    update() {
+        if (this.x <= -this.width) this.x = 0;
+
+        this.x -= this.game.speed * this.speedModifer;
+    }
+
+    draw(context) {
+        context.drawImage(this.image, this.x, this.y);
+        context.drawImage(this.image, this.x + this.width, this.y);
+    }
 }
